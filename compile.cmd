adb850e249816d415b4bf89efef48970d7aeb534
0aen0 <asoskoven@gmail.com>
2026-03-08 05:07:34 +0300

    Update files

---
+++ compile.cmd
@@ -0,0 +1,2 @@
+python -m nuitka --onefile --windows-console-mode=force --standalone --enable-plugin=tk-inter --copyright="AEN 2024-2025." --file-version="2.0.2" --product-name="SOP utility" .\sop-utility.py
+#python -m nuitka --onefile --windows-console-mode=disable --standalone --enable-plugin=tk-inter --copyright="AEN 2024-2025." --file-version="2.0.2" --product-name="SOP utility" .\sop-utility-gui.py
